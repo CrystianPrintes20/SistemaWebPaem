f454d04594f21294cd6c31625f8d84a7493fd4b5
CrystianPrinte20 <63503019+CrystianPrintes20@users.noreply.github.com>
2021-10-05 20:24:08 -0300

    cpf validation

--- View/discente/cadastrar_disc.php
+++ View/discente/cadastrar_disc.php
@@ -87,7 +87,7 @@ session_start()
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" >CPF</span>
                                     </div>
-                                    <input required name="cpf" id="cpf" type="text" class="form-control" placeholder="Digite seu numero do CPF SEM OS PONTOS" aria-label="cpf" aria-describedby="basic-addon5" maxlength="13" onkeypress="$(this).mask('000.000.000-09')">
+                                    <input required name="cpf" id="cpf" type="text" class="form-control" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})" placeholder="Digite seu numero do CPF SEM OS PONTOS" aria-label="cpf" aria-describedby="basic-addon5" maxlength="14" onkeypress="$(this).mask('000.000.000-09')" onblur="validarCPF(this)">
                                 </div>
 
                                 <!--Campus
@@ -514,6 +514,49 @@ $(document).ready(function () {
 // }
 // }
 </script>
-
+<script>
+    function validarCPF(el){
+        if( !_cpf(el.value) ){
+        alert("CPF inválido!" + el.value);
+        // apaga o valor
+        el.value = "";
+        }
+    }
+</script>
+<script>
+    function _cpf(cpf) {
+        cpf = cpf.replace(/[^\d]+/g, '');
+        if (cpf == '') return false;
+        if (cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999")
+        return false;
+        add = 0;
+        for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+        rev = 11 - (add % 11);
+        if (rev == 10 || rev == 11)
+        rev = 0;
+        if (rev != parseInt(cpf.charAt(9)))
+        return false;
+        add = 0;
+        for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+        rev = 11 - (add % 11);
+        if (rev == 10 || rev == 11)
+        rev = 0;
+        if (rev != parseInt(cpf.charAt(10)))
+        return false;
+        return true;
+    }
+</script>
 </body>
 </html>
