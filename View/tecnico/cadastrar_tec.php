29e192c8008365ecf552f075e736807c75e57408
CrystianPrintes20 <63503019+CrystianPrintes20@users.noreply.github.com>
2021-10-08 17:57:08 -0300

    Remove bug espaços

--- View/tecnico/cadastrar_tec.php
+++ View/tecnico/cadastrar_tec.php
@@ -280,26 +280,5 @@ session_start();
     }
 </script>
 
-<script>
-    function ApenasLetras(e, t) {
-        try {
-            if (window.event) {
-                var charCode = window.event.keyCode;
-            } else if (e) {
-                var charCode = e.which;
-            } else {
-                return true;
-            }
-            if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123))
-                return true;
-            else
-                return false;
-        } catch (err) {
-            alert(err.Description);
-        }
-    }
-
-</script>
-
 </body>
 </html>
