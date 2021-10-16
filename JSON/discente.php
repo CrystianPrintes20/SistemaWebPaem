1a1dab1f6248d888b8e19f3eb4631d53f604e8c8
CrystianPrinte20 <63503019+CrystianPrintes20@users.noreply.github.com>
2021-10-16 14:56:29 -0300

    add rota, tecnicos, docentes e etc no json

---
+++ JSON/discente.php
@@ -0,0 +1,202 @@
+<?php
+$discente = array(
+    'unidade' => array(
+        'CALE' => array(
+        
+        ),
+    
+        'CITB' => array(
+            
+        ),
+    
+        'CJUR' => array(
+        
+        ),
+    
+        'CMAL' => array(
+            
+        ),
+        'CORI' => array(
+            
+        ),
+    
+        'COBI' => array(
+            
+        ),
+    
+        'CFI' => array(
+            
+        ),
+        'CGIFP' => array(
+            
+        ),
+    
+        'PARFOR' => array(
+            
+        ),
+
+        'IBEF' => array(
+            '2011' => array(
+
+                'AGRONOMIA_BACHARELADO' => array(
+                    '201100789' => 'DANIEL CARVALHO EVANGELISTA'
+                ),
+
+                'ENGENHARIA_FLORESTAL_BACHARELADO' => array(
+                    '201100530' => 'LUIS PAULO DA SILVA LIMA',
+                    '201100390' => 'MARIANE CAROLINA FERREIRA DE SOUSA',
+                    '201100676' => 'PEDRO IVES SOUZA'
+                ),
+
+                'INTERDISCIPLINAR_EM_CIENCIAS_AGRARIAS_BACHARELADO' => array(
+                    '201100394' => 'CRISTIANE FERREIRA SERRA'
+                )
+            ),
+            
+        ),
+
+        'ICED' => array(
+            '2011' => array(
+                'LETRAS_PORTUGUES_E_INGLES_LICENCIATURA' => array(
+                    '201100959' => 'ANA CLAUDIA MEDEIROS DA SILVA',
+                    '201100820' => 'CLAUDIA DANIELA MOTA AVELINO',
+                    '201101115' => 'GENIVAL DANIEL GODINHO SOUSA',
+                    '201101117' => 'JOÃO TOBIAS ASSUNÇÃO DE SOUSA',
+                    '201101174' => 'MARIVANIA CORREA MENDES REBELO'
+                ),
+
+                'LICENCIATURA_INTEGRADA_EM HISTORIA_EGEOGRAFIA' => array(
+                    '201100361' =>	'ANDREI NUNES FIGUEIRA',
+                    '201100624' =>	'PATRICK PEREIRA DE SOUZA',
+                    '201100697' =>	'ANDRE WILLIAN BENTES DOS SANTOS',
+                    '201100799' =>	'GEORGE EMILIO RODRIGUES LIMA',
+                    '201100791'	=>'JAQUELINE DOS SANTOS FIGUEIRA',
+                    '201100485'	=>'LUCIANO PEREIRA ALVES',
+                    '201100645'	=>'NARCISO JOSE FONSECA DE SENNA PEREIRA'
+                ),
+
+                'LICENCIATURA_INTEGRADA_EM_MATEMÁTICA_E_FISICA' => array(
+                    '201100562'=>	'ADRIA KARINE DE JESUS NASCIMENTO',
+                    '201100414'=>	'RODRIGO MARTINS DA SILVA'
+                ),
+
+                'PEDAGOGIA_LICENCIATURA' => array(
+                    '201100744'=>	'DANIELLY MOREIRA PEREIRA',
+                    '201101004'=>	'DENILSON COSTA BORGES',
+                    '201101167'=>	'ENDRIA CARLA BRITO MAGALHAES',
+                    '201100401'=>	'RENILSON MOTA DA SILVA'
+                )
+            )
+        ),
+
+        'ICS' => array(
+            '2011' => array(
+                //curso: ANTROPOLOGIA_BACHARELADO
+                '5' => array(
+                    '201100730'=>	'ALINE SUELEM MAIA SANTOS',
+                    '201100444'=>	'GABRIELA NEVES LOPES',
+                    '201100751'=>	'JULIETE CRISTINA LOBO RODRIGUES',
+                    '111111111' =>  'ANTONIO MARIA',
+                    '222222222' =>  'JOÃO RODRIGUES'
+                ),
+
+             
+                'CIÊNCIAS ECONÔMICAS' => array(
+                    '201100580'=>	'AGEU PANTOJA GUIMARÃES',
+                    '201101099'=>	'AIANE RIBEIRO SANTOS',
+                    '201100966'=>	'ALINE CARLA MATOS BENTES',
+                    '201101056'=>	'EDJANE VASCONCELOS FERNANDES',
+                    '201100274'=>	'EVERTON JOSE AMARAL PEREIRA',
+                    '201100848'=>	'FLAVIO ROBERTO CARVALHO MENEZES',
+                    '201100824'=>	'FRANCINELSON CARVALHO MENEZES',
+                    '201101078'=>	'MARLON SILVA CALANDRINI',
+                    '201100721'=>	'RAFAEL REBELO LOPES',
+                    '201100429'=>	'THACY CRUZ TRINDADE'
+                ),
+    
+                'DIREITO_BACHARELADO' => array(
+                    '201100113' =>	'HECTOR BRUNO FERREIRA PAXIUBA',
+                    '201100058' =>	'KATIA CRISTINA LIRA SATO',
+                    '201100469' =>	'MARCELO JOSE DE SOUSA RABELO'
+                ),
+    
+                'GESTAO_PUBLICA_E_DESENVOLVIMENTO_REGIONAL_BACHARELADO' => array(
+                    '201100488'=>	'EDIVANIA LOPES SILVA',
+                    '201100105'=>	'EMERSON RIBEIRO VIEIRA',
+                    '201100320'=>	'JUCERVANDRO VIANA DOS SANTOS JUNIOR',
+                    '201100643'=>	'MIZAEL SANTOS DOS SANTOS'
+                )
+            )
+            
+        ),
+
+        'ICTA' => array(
+            '2011' => array(
+                'CIENCIAS_BIOLOGICAS_BACHARELADO' => array(
+                    '201100978' =>	'DIEGO PATRICK SOUSA GODINHO',
+                    '201100800' =>	'THAIS DA SILVA FROES'
+                ),
+    
+                'ENGENHARIA_DE_PESCA_BACHARELADO' => array(
+                    '201100598' =>	'ANA MARCIA NEVES BASTOS',
+                    '201100969' =>	'DORIVALDO BATISTA FIGUEIRA JUNIOR',
+                    '201101175'	=> 'LUSIA LEITE DE OLIVEIRA'
+                ),
+    
+                'INTERDISCIPLINAR_EM_CIENCIAS_E_TECNOLOGIA_DAS_AGUAS_BACHARELADO' => array(
+                    '201100625' =>	'IOMAR NASCIMENTO DOS SANTOS'
+                )
+            )
+            
+        ),
+
+        'IEG' => array(
+            '2011' => array(
+                //CIENCIA_DA_COMPUTAÇÃO_BACHARELADO
+                '7' => array(
+                    '201100868' =>	'CARLOS RODOLFO OLIVEIRA DOS SANTOS',
+                    '201100550' =>	'DAILSON MELO CAMPOS',
+                    '201100349' =>	'ELVIS RIBEIRO DA SILVA',
+                    '201100904' =>	'ROBERTO COELHO DE AMORIM'
+                ),
+    
+                'ENGENHARIA_FISICA_BACHARELADO' => array(
+                    '201100063' =>	'ADRIANO AUGUSTO ILENO FONTENELE',
+                    '201100245' =>	'FABRICIO AUGUSTO SOUSA DA SILVA',
+                    '201100083' =>	'RAFAEL REBELO PANTOJA'
+                ),
+    
+                'GEOFISICA_BACHARELADO' => array(
+                    '201100736' =>	'DAIANE XAVIER MONTELES'
+                ),
+                
+                'INTERDISCIPLINAR_EM_CIENCIAS_E_TECNOLOGIA_BACHARELADO' => array(
+                    '201101130' =>	'ARLLEY JACKSON PRATES QUEIROZ',
+                    '201100456' =>	'ELVIS BERNARDO BARBOSA MELO',
+                    '201100505' =>	'FRANCISCO ROBSON ALVES DA SILVA',
+                    '201101003' =>	'PABLO DOS SANTOS',
+                    '201100541' =>	'RHUAM DHIANGELO DIAS BARBOSA',
+                    '201100581' =>	'SAMARA MORENO NASCIMENTO'
+                )
+            )
+            
+        ),
+
+        'ISCO' => array(
+            '2011' => array(
+                'FARMACIA_BACHARELADO' => array(
+                    '201100710' =>	'BRUNA RODRIGUES DA SILVA',
+                    '201100878' =>	'GILIANDER SILVA DOS SANTOS'
+                )
+            )     
+        )
+
+    )    
+  
+);
+
+$discente_json = json_encode($discente, JSON_UNESCAPED_UNICODE);
+
+echo $discente_json;
+
+?>
---
+++ JSON/rota_api.php
@@ -0,0 +1,4 @@
+<?php
+$rotaApi = 'http://127.0.0.1:5000';
+//http://webservicepaem-env.eba-mkyswznu.sa-east-1.elasticbeanstalk.com
+?>
