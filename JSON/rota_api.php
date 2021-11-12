b4d3e7c4918afd6cd598bd4f13fa25bc15b282d8
CrystianPrinte20 <63503019+CrystianPrintes20@users.noreply.github.com>
2021-11-12 17:14:03 -0300

    route

--- JSON/rota_api.php
+++ JSON/rota_api.php
@@ -1,5 +1,5 @@
 <?php
-$rotaApi = 'http://127.0.0.1:5000';
-//$rotaApi = 'http://webservicepaem-env.eba-mkyswznu.sa-east-1.elasticbeanstalk.com';
+//$rotaApi = 'http://127.0.0.1:5000';
+$rotaApi = 'http://webservicepaem-env.eba-mkyswznu.sa-east-1.elasticbeanstalk.com';
 //http://webservicepaem-env.eba-mkyswznu.sa-east-1.elasticbeanstalk.com
 ?>
